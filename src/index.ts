708c5141daf19e724360711f6ccb2f9986d9624e
knock-huynh <huynhtransongngoc@gmail.com>
2025-02-25 23:50:54 +0700

    feat(stream-text): add stream text demo

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
-import { google } from "@ai-sdk/google";
-import { generateText } from "ai";
-
-const model = google("gemini-1.5-flash");
-
-const text = await generateText({
-  model,
-  prompt: "Write a vegetarian lasagna recipe for 4 people.",
-});
-
-console.log(text);
-
 import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
+import { sendPrompt } from "./util";
+import { streamText } from "hono/streaming";
+
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hono!"));
+app.use(logger());
+app.use(prettyJSON());
+
+const globalSessionID = "550b0944-681d-4c35-ab43-3be1bd521b6f";
+app.post("/prompt", async (c) => {
+  const body = await c.req.json();
+  const sessionID = body.sessionID;
+  const userInput = body.userInput;
+
+  if (sessionID !== globalSessionID) {
+    return c.json({ error: "Not found sessionID" });
+  }
+
+  return streamText(c, async (stream) => {
+    await sendPrompt(stream, userInput);
+  });
+});
 
 export default app;
--- src/util.ts
+++ src/util.ts
@@ -1,9 +1,26 @@
 import { google } from "@ai-sdk/google";
-import { generateText } from "ai";
+import { streamText, type CoreMessage } from "ai";
+import type { StreamingApi } from "hono/utils/stream";
 
 const model = google("gemini-1.5-flash");
 
-const text = await generateText({
-  model,
-  prompt: "Write a vegetarian lasagna recipe for 4 people.",
-});
+const messages: CoreMessage[] = [];
+
+export const sendPrompt = async (stream: StreamingApi, userInput: string) => {
+  messages.push({ role: "user", content: userInput });
+
+  const result = streamText({
+    model,
+    messages,
+  });
+
+  let fullResponse = "";
+
+  for await (const delta of result.textStream) {
+    fullResponse += delta;
+    await stream.write(delta);
+  }
+
+  messages.push({ role: "assistant", content: fullResponse });
+  await stream.close();
+};
