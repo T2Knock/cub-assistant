67cc4505e26d1fa8b346e9382295b3617b7f824c
knock-huynh <huynhtransongngoc@gmail.com>
2025-02-25 22:20:06 +0700

    feat(ai): add vercel ai sdk

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { google } from "@ai-sdk/google";
+import { generateText } from "ai";
+
+const model = google("gemini-1.5-flash");
+
+const text = await generateText({
+  model,
+  prompt: "Write a vegetarian lasagna recipe for 4 people.",
+});
+
+console.log(text);
